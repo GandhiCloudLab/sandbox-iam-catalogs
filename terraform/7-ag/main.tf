cc0c5c9e92f8543d4910c9a7967c059bd0845980
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-11 16:52:53 +0530

    Script Updated

--- terraform/7-ag/main.tf
+++ terraform/7-ag/main.tf
@@ -10,7 +10,7 @@ data "ibm_iam_access_group" "accgrp123" {
   access_group_name = "APPDEV-ENVIRONMENT-ADMINS"
 }
 
-# resource "ibm_iam_access_group_members" "res_groupmem_admins" {
-#   access_group_id = "${data.ibm_iam_access_group.accgrp123.id}"
-#   ibm_ids         =  ["${var.admin_id}"]
-# }
+resource "ibm_iam_access_group_members" "res_groupmem_admins" {
+  access_group_id = "${data.ibm_iam_access_group.accgrp123.groups.0.id}"
+  ibm_ids         =  ["${var.admin_id}"]
+}
