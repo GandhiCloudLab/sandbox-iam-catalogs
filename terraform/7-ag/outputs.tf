34cf24aed61532718ba582da3fd50fa4b143cbd8
GandhiCloudLab <GandhiCloudLab@gmail.com>
2020-05-11 16:22:40 +0530

    Script Updated

---
+++ terraform/7-ag/outputs.tf
@@ -0,0 +1,3 @@
+output "accessGroup1"{
+	value = [ "$(ibm_iam_access_group.res_ag)" ]
+}
---
+++ terraform/7-ag/variables.tf
@@ -0,0 +1,8 @@
+variable user_access_groups{
+	default = ["APPDEV-USERS1", "APPDEV-USERS2"]
+}
+
+variable admin_id{
+	type = "string"
+    default = "dimallya@in.ibm.com"
+}
